2d1de683300978e073d72ce416ae4809bc811983
accraze <accraze@gmail.com>
2015-09-26 15:33:24 -0700

    splits each line into tokens

---
+++ src/index.js
@@ -0,0 +1,40 @@
+
+module.exports = {
+    text2token: text2token
+}
+
+Array.prototype.contains = function(v) {
+    for(var i = 0; i < this.length; i++) {
+        if(this[i] === v) return true;
+    }
+    return false;
+};
+
+Array.prototype.unique = function() {
+    var arr = [];
+    for(var i = 0; i < this.length; i++) {
+        if(!arr.contains(this[i])) {
+            arr.push(this[i]);
+        }
+    }
+    return arr; 
+}
+
+function text2token(textFile) {
+    var lines = [],
+        tokens = [];
+
+    var fs = require('fs');
+    var lines = fs.readFileSync(textFile).toString().split("\n");
+
+    for (var i = 0; i < lines.length; i++) {
+        var words = lines[i].split(" ");
+        for (var j = 0; j < words.length; j++) {
+            tokens.push(words[j]);
+        }
+    }
+
+    tokens = tokens.unique();
+
+    return {lines: lines, tokens:tokens}
+}
