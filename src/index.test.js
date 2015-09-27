6de7cd42436c06369d734d669cbd1d5302b5bbe8
accraze <accraze@gmail.com>
2015-09-26 19:50:51 -0700

    test(text2token): should throw an error if no path is given
    For this method to work, we need a valid path to a text file. Node's FS will throw an error if the
    path does not lead to a valid text file. If a path is not given or is not in proper format, it will
    throw an error

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,11 @@
 var expect = require('chai').expect;
 var lib = require('./index');
 
-describe('text2token', function(){
+describe('text2token', function() {
+  it('should throw an error if no path is given', function () {
+    expect(lib.text2token).to.throw('path is required');
+  });
+  
   it('should return an object of lines and tokens', function () {
     var conversion = lib.text2token('./src/bigtext.txt');
 
